d9c078986ceaa365e8917b2e574f36619516a23f
GsLee <lgswin@gmail.com>
2025-02-18 00:54:34 -0500

    add tests

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoModelTestCase(TestCase):
+    """ Unit test for Todo model """
+
+    def setUp(self):
+        """ Create a sample Todo object before each test """
+        self.todo = Todo.objects.create(
+            title="Test Todo",
+            description="This is a test todo item",
+            completed=False
+        )
+
+    def test_todo_creation(self):
+        """ Test if the Todo object is created correctly """
+        self.assertEqual(self.todo.title, "Test Todo")
+        self.assertEqual(self.todo.description, "This is a test todo item")
+        self.assertFalse(self.todo.completed)
+
+    def test_todo_update(self):
+        """ Test if the Todo object can be updated """
+        self.todo.title = "Updated Todo"
+        self.todo.completed = True
+        self.todo.save()
+
+        updated_todo = Todo.objects.get(id=self.todo.id)
+        self.assertEqual(updated_todo.title, "Updated Todo")
+        self.assertTrue(updated_todo.completed)
+
+    def test_todo_deletion(self):
+        """ Test if the Todo object can be deleted """
+        todo_id = self.todo.id
+        self.todo.delete()
+        with self.assertRaises(Todo.DoesNotExist):
+            Todo.objects.get(id=todo_id)
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -10,14 +10,14 @@ class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
 
-    @method_decorator(csrf_exempt)  # ✅ CSRF 예외 적용
+    @method_decorator(csrf_exempt)  # CSRF Exception
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
-    @method_decorator(csrf_exempt)  # ✅ CSRF 예외 적용
+    @method_decorator(csrf_exempt)  # CSRF Exception
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
-    @method_decorator(csrf_exempt)  # ✅ CSRF 예외 적용
+    @method_decorator(csrf_exempt)  # CSRF Exception
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
